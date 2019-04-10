50292a12703a4f81b4de46b798bde951266b9d3e
ghous <ghousadnan62@gmail.com>
2019-04-09 23:46:09 -0700

    Responsiveness added to icons component

--- src/Components/HomeScreenComps/ServicesBriefIntro.js
+++ src/Components/HomeScreenComps/ServicesBriefIntro.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../../App.css'
 import IconComp from '../Utilities/IconComp'
-
+import MediaQuery from 'react-responsive';
+import {ScreenBreakPoint1} from '../Utilities/ScreenBreakPoint';
 
 import {
     faDraftingCompass,
@@ -19,73 +20,140 @@ import {
 
 const ServicesBriefIntro = () => {
     return (
+        <div>
+            {/*//Media Query for Laptops*/}
+            <MediaQuery minWidth={ScreenBreakPoint1}>
+                <div className={'servicesInfoWrapper themeBackground'}
+                     style={{
+                         height: `${window.innerHeight}px`
+                     }}
+                >
 
-        <div className={'servicesInfoWrapper themeBackground'}
-            style={{
-                height:`${window.innerHeight}px`
-            }}
-        >
-
-            <div className={'titleStyling themeFontColor'}>
-                Our Services
-            </div>
+                    <div className={'titleStyling themeFontColor'}>
+                        Our Services
+                    </div>
 
 
-            <div className={'iconsWrapper'}>
+                    <div className={'iconsWrapper'}>
 
-                <div className={'iconsStyling'}>
-                    <IconComp
-                        iconName={faDraftingCompass}
-                        titleText={'Designing'}
-                    />
-                    <IconComp
-                        iconName={faBuilding}
-                        titleText={'Infrastructure'}
-                    />
+                        <div className={'iconsStyling'}>
+                            <IconComp
+                                iconName={faDraftingCompass}
+                                titleText={'Designing'}
+                            />
+                            <IconComp
+                                iconName={faBuilding}
+                                titleText={'Infrastructure'}
+                            />
 
-                    <IconComp
-                        iconName={faDoorOpen}
-                        titleText={'Interior Works'}
-                    />
+                            <IconComp
+                                iconName={faDoorOpen}
+                                titleText={'Interior Works'}
+                            />
+                        </div>
+
+
+                        <div className={'iconsStyling'}>
+                            <IconComp
+                                iconName={faUserShield}
+                                titleText={'Supervision'}
+                            />
+                            <IconComp
+                                iconName={faTree}
+                                titleText={'Landscaping'}
+                            />
+
+                            <IconComp
+                                iconName={faHardHat}
+                                titleText={'Construction'}
+                            />
+                        </div>
+
+
+                        <div className={'iconsStyling'}>
+                            <IconComp
+                                iconName={faHandshake}
+                                titleText={'Consultancy'}
+                            />
+                            <IconComp
+                                iconName={faKey}
+                                titleText={'Turnkey'}
+                            />
+
+                            <IconComp
+                                iconName={faLeaf}
+                                titleText={'Green Design'}
+                            />
+                        </div>
+
+                    </div>
                 </div>
+            </MediaQuery>
 
 
-                <div className={'iconsStyling'}>
-                    <IconComp
-                        iconName={faUserShield}
-                        titleText={'Supervision'}
-                    />
-                    <IconComp
-                        iconName={faTree}
-                        titleText={'Landscaping'}
-                    />
+           {/* -----------------------------------------------------------------
+            //Media Query for Mobiles
+            //Here the icons components are 6
+            instead of 9
+            ------------------------------------------------------------------*/}
 
-                    <IconComp
-                        iconName={faHardHat}
-                        titleText={'Construction'}
-                    />
+            <MediaQuery maxWidth={ScreenBreakPoint1}>
+                <div className={'servicesInfoWrapper themeBackground'}
+                     style={{
+                         height: `${window.innerHeight}px`
+                     }}
+                >
+
+                    <div className={'titleStyling themeFontColor'}>
+                        Our Services
+                    </div>
+
+
+                    <div className={'iconsWrapper'}>
+
+                        <div className={'iconsStyling'}>
+                            <IconComp
+                                iconName={faDraftingCompass}
+                                titleText={'Designing'}
+                            />
+                            <IconComp
+                                iconName={faBuilding}
+                                titleText={'Infrastructure'}
+                            />
+
+                        </div>
+
+
+                        <div className={'iconsStyling'}>
+                            <IconComp
+                                iconName={faUserShield}
+                                titleText={'Supervision'}
+                            />
+
+                            <IconComp
+                                iconName={faHardHat}
+                                titleText={'Construction'}
+                            />
+                        </div>
+
+
+                        <div className={'iconsStyling'}>
+                            <IconComp
+                                iconName={faHandshake}
+                                titleText={'Consultancy'}
+                            />
+                            <IconComp
+                                iconName={faKey}
+                                titleText={'Turnkey'}
+                            />
+                        </div>
+
+                    </div>
                 </div>
+            </MediaQuery>
 
-
-
-                <div className={'iconsStyling'}>
-                    <IconComp
-                        iconName={faHandshake}
-                        titleText={'Consultancy'}
-                    />
-                    <IconComp
-                        iconName={faKey}
-                        titleText={'Turnkey'}
-                    />
-
-                    <IconComp
-                        iconName={faLeaf}
-                        titleText={'Green Design'}
-                    />
-                </div>
-
-            </div>
         </div>
+
 
     );
 };
