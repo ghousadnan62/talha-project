3854db2bc43f29f116f1cf0a9ae8bd9dd0964787
ghous <ghousadnan62@gmail.com>
2019-04-10 22:53:34 -0700

    Contact US card Completed

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 import '../App.css'
-import Header from '../Components/HomeScreenComps/HeaderFooter/Header';
+import Header from '../Components/HomeScreenComps/Header/Header';
 import Slider from '../Components/Slider/index';
 import ServicesIntro from '../Components/HomeScreenComps/ServicesBriefIntro'
 import FooterComp from "../Components/HomeScreenComps/FooterComp";
 import AboutUsComp from '../Components/HomeScreenComps/AboutUsComp';
-
 
 
 export default class HomeScreen extends Component {
